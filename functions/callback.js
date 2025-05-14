5bae6dc89a7f62cfd266f2d808ff095600dadca9
franzuuia <frankzuuia@gmail.com>
2025-05-14 15:49:36 -0600

    Agrega función callback para recibir el código de Mercado Libre

---
+++ functions/callback.js
@@ -0,0 +1,8 @@
+export async function onRequestGet(context) {
+  const { request } = context;
+  const url = new URL(request.url);
+  const code = url.searchParams.get('code');
+  return new Response(`¡Callback recibido! Código: ${code}`, {
+    headers: { 'content-type': 'text/plain' },
+  });
+}
